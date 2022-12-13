a48de8c87ada21efde1e83f7b69455cd1956791c
jessie570314 <jessie570314@gmail.com>
2022-12-14 03:21:55 +0800

    New room

--- Assets/Scripts/Room1/Coffin.cs
+++ Assets/Scripts/Room1/Coffin.cs
@@ -14,6 +14,7 @@ public class Coffin : MonoBehaviour
     {
         renderer = GetComponent<Renderer>();
         click_coffin = false;
+        MouseIn = false;
         coffin = this.gameObject.transform.parent.gameObject;
         coffinanimation = coffin.GetComponent<CoffinAnimation>();
     }
