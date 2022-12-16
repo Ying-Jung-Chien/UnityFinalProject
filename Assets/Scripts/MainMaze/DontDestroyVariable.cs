c0c24f330a8486352554497eaef2b9d7d7366272
Ding <henry0125xu@gmail.com>
2022-12-16 20:59:34 +0800

    fix door2 ui

--- Assets/Scripts/MainMaze/DontDestroyVariable.cs
+++ Assets/Scripts/MainMaze/DontDestroyVariable.cs
@@ -27,7 +27,7 @@ public class DontDestroyVariable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        manager.resetbag();
+        //manager.resetbag();
     }
 
     // Update is called once per frame
