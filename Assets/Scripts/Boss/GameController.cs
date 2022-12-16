d3790008739abf9ff894ee0fb9e8640fd42943a2
Lyc <steven031186@gmail.com>
2022-12-16 22:43:15 +0800

    Add Boss fire ball.

--- Assets/Scripts/Boss/GameController.cs
+++ Assets/Scripts/Boss/GameController.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject player;
+    public GameObject playerHead;
     public GameObject blackDragon;
+    public GameObject blackDragonHead;
+    public GameObject blackDragonHeadFront;
+    public GameObject bossFireBall;
 
     private bool firstScream;
+
     // Start is called before the first frame update
     void Start()
     {
         firstScream = true;
+        //firstScream = false;
     }
 
     // Update is called once per frame
@@ -21,13 +27,13 @@ public class GameController : MonoBehaviour
         {
             //保存碰撞信息
             RaycastHit m_hit;
-            Ray ray = new Ray(blackDragon.transform.position, player.transform.position - blackDragon.transform.position);
-            if (Physics.Raycast(ray, out m_hit, Vector3.Distance(blackDragon.transform.position, player.transform.position)))
+            Ray ray = new Ray(blackDragonHead.transform.position, playerHead.transform.position - blackDragonHead.transform.position);
+            if (Physics.Raycast(ray, out m_hit, Vector3.Distance(blackDragonHead.transform.position, playerHead.transform.position)))
             {
                 if (m_hit.transform.tag != "Player" && m_hit.transform.name != "Boss")
                 {
                     //Debug.Log("Collider name: " + m_hit.transform.name);
-                    Debug.DrawLine(blackDragon.transform.position, player.transform.position, Color.red);
+                    Debug.DrawLine(blackDragonHead.transform.position, playerHead.transform.position, Color.red);
                 }
                 else
                 {
@@ -40,6 +46,13 @@ public class GameController : MonoBehaviour
                 Boss.goScream = true;
                 firstScream = false;
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject fireball = Instantiate(bossFireBall, blackDragonHeadFront.transform.position, Quaternion.Euler(180, 0, 0));
+            //GameObject fireball = Instantiate(bossFireBall, blackDragonHeadFront.transform.position, Quaternion.identity);
+            fireball.GetComponent<BossFireBallController>().Init(blackDragonHead.transform.position, blackDragonHeadFront.transform.position, player.transform.position);
         }
     }
 }
