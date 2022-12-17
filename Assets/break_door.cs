8596b8f3cdc3352dae27dd42cb971aff4e71f481
Rakson0209 <72456829+Rakson0209@users.noreply.github.com>
2022-12-17 17:05:34 +0800

    door_test

---
+++ Assets/break_door.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class break_door : MonoBehaviour
+{
+    public GameObject R_door;
+    public GameObject L_door;
+    float rotateSpeed = 2f;
+    Quaternion targetAngels_R;
+    Quaternion targetAngels_L;
+
+    public bool door_open = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAngels_R = Quaternion.Euler(0, -113.1f, 0);
+        targetAngels_L = Quaternion.Euler(0, 98.9f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (door_open == true)
+        {
+            if (R_door.transform.eulerAngles.y > -113.1f) { R_door.transform.Rotate(0, -1, 0); }
+            if (L_door.transform.eulerAngles.y < 98.9f) { L_door.transform.Rotate(0, 1, 0); }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == R_door || other == L_door || other.tag == "enemy")
+        {
+            door_open = true;
+        }
+    }
+}
