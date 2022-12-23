b485f8ea37754b7f258b7826515f3b853f00e085
Lyc <steven031186@gmail.com>
2022-12-23 14:19:34 +0800

    Add the animation of the boss when it get heart.

--- Assets/Scripts/Boss/GameController.cs
+++ Assets/Scripts/Boss/GameController.cs
@@ -9,20 +9,70 @@ public class GameController : MonoBehaviour
     public GameObject blackDragon;
     public GameObject blackDragonHead;
     public GameObject blackDragonHeadFront;
-    public GameObject bossFireBall;
+    public GameObject bossBlood;
+
+    public static bool stopBoss = true;
 
     private bool firstScream;
+    private bool test = false;
 
     // Start is called before the first frame update
     void Start()
     {
         firstScream = true;
+        test = false;
         //firstScream = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            test = !test;
+            if (test)
+            {
+                Debug.Log("Testing state");
+                boss_entrance.trap_activate = false;
+                player.GetComponent<CharacterController>().enabled = false;
+                player.transform.position = new Vector3(0, -1, -25);
+                Invoke("ActiveCharacterController", 0.1f);
+                
+            }
+            else
+            {
+                Debug.Log("Not testing state.");
+                boss_entrance.trap_activate = true;
+                blackDragon.SetActive(false);
+                bossBlood.SetActive(false);
+                player.GetComponent<CharacterController>().enabled = false;
+                player.transform.position = new Vector3(0, -6.75f, -80f);
+                Invoke("ActiveCharacterController", 0.1f);
+            }
+        }
+        if(test)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                blackDragon.SetActive(false);
+                bossBlood.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                blackDragon.SetActive(true);
+                bossBlood.SetActive(true);
+            }
+            if(Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Boss.goFly = true;
+                blackDragon.GetComponent<Animator>().SetBool("goFly", true);
+            }
+            if(Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                stopBoss = !stopBoss;
+            }
+        }
+
         if (firstScream)
         {
             //保存碰撞信息
@@ -47,11 +97,10 @@ public class GameController : MonoBehaviour
                 firstScream = false;
             }
         }
+    }
 
-        //if (Input.GetKeyDown(KeyCode.F))
-        //{
-        //    GameObject fireball = Instantiate(bossFireBall, blackDragonHeadFront.transform.position, Quaternion.LookRotation(blackDragonHeadFront.transform.position - blackDragonHead.transform.position));
-        //    fireball.GetComponent<BossFireBallController>().Init(blackDragonHead.transform.position, blackDragonHeadFront.transform.position, player.transform.position);
-        //}
+    void ActiveCharacterController()
+    {
+        player.GetComponent<CharacterController>().enabled = true;
     }
 }
