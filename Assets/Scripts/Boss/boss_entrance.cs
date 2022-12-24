b1f8d4ccc8ce906c4eab8065e4136ba80c5f4285
Rakson0209 <72456829+Rakson0209@users.noreply.github.com>
2022-12-25 01:22:56 +0800

    破門bug

--- Assets/Scripts/Boss/boss_entrance.cs
+++ Assets/Scripts/Boss/boss_entrance.cs
@@ -7,6 +7,10 @@ public class boss_entrance : MonoBehaviour
     public GameObject trap_ball;
     private GameObject trap_ball_clone;
     private float start_time;
+    public GameObject player;
+
+    public GameObject R_door;
+    public GameObject L_door;
 
     public static bool trap_activate = true;
 
@@ -26,6 +30,11 @@ public class boss_entrance : MonoBehaviour
             {
                 generate_trap_ball();
             }
+        }
+        if (player.GetComponent<break_door>().door_open)
+        {
+            if (R_door.transform.eulerAngles.y > 246.9f || R_door.transform.eulerAngles.y == 0) { R_door.transform.Rotate(0, -1, 0); }
+            if (L_door.transform.eulerAngles.y < 98.9f) { L_door.transform.Rotate(0, 1, 0); }
         }
     }
 
