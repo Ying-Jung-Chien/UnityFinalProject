03b7be5c705dcdf99572f7901f5093ef6206fda9
Ying-Jung-Chien <64266416+Ying-Jung-Chien@users.noreply.github.com>
2023-01-13 21:42:54 +0800

    fix wall & add player blood

--- Assets/Scripts/MainMaze/DontDestroyVariable.cs
+++ Assets/Scripts/MainMaze/DontDestroyVariable.cs
@@ -7,7 +7,7 @@ public class DontDestroyVariable : MonoBehaviour
     public static int[] getPuzzle = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
     public static int[] getNinePuzzle = new int[] {0, 0, 0, 0, 0};
     public static int lastRoom = 0;
-    public static float PlayerHealth = 100.0f;
+    public static float PlayerHealth = 1000.0f;
     public static float PlayerBlue = 100.0f;
     public static bool isMonsterADead = false;
     public static bool isMonsterBDead = false;
